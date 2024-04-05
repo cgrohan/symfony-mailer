d30c04b3f866564aecbf7dff55a391b682e7cc6c
Clément GROHAN <clement.grohan@mes-services.me>
2024-04-05 15:39:37 +0200

    ajoutement des validations

--- src/DTO/ContactDTO.php
+++ src/DTO/ContactDTO.php
@@ -7,7 +7,12 @@ use Symfony\Component\Validator\Constraints as Assert;
 class ContactDTO
 {
     #[Assert\NotBlank]
-    #[Assert\Length(min: 2)]
+    #[Assert\Length(
+        min: 2,
+        max: 30,
+        minMessage:'Ton prénom comprend réellement une lettre ?',
+        maxMessage:'Nous trouvons que la limite de {{ limit }} caractères est déjà beaucoup trop élévé.'
+    )]
     public string $name;
 
     #[Assert\NotBlank]
@@ -15,6 +20,11 @@ class ContactDTO
     public string $email;
 
     #[Assert\NotBlank]
-    #[Assert\Length(min: 20, max: 1000)]
+    #[Assert\Length(
+        min: 20,
+        max: 1000,
+        minMessage:'Merci d\'écrire plus d\'un mot :) au moins {{ limit }} caractères.',
+        maxMessage:'Nos équipes ne sont pas payés pour lire des histoires :D merci de ne pas dépasser {{ limit }} caractères.'
+    )]
     public string $message;
 }
