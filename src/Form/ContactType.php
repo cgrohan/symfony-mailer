9b9b102336b560f1bd81b3c61817f1da20950944
Clément GROHAN <clement.grohan@mes-services.me>
2024-04-05 12:46:00 +0200

    Création du formulaire DTO

---
+++ src/Form/ContactType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ContactType extends AbstractType
+{
+    #[Assert\NotBlank]
+    public string $nom;
+    #[Assert\NotBlank]
+    public string $email;
+    #[Assert\NotBlank]
+    public string $message;
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('email', EmailType::class )
+            ->add('message', TextareaType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+
+    public function getName(): string
+    {
+        return $this->nom;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
